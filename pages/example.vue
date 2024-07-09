7abbb81ba13a61c01c296367a7242c3bb9af3c54
Bureaupixel <bram.overstijns@gmail.com>
2024-07-09 21:40:07 +0200

    first upload

---
+++ pages/example.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <section class="relative h-screen flex flex-col text-white top-0">
+     <ClientOnly> 
+     <video-background 
+         id="myVideo"
+         src="/video/V09-0001-0500.mp4"
+         poster="/images/prev1.jpg"
+         overlay="linear-gradient(to right, zinc-900, slate-900)"
+         class="fixed top-0 left-0 w-full h-full overflow-hidden mt-5">
+         <h1 class="font-extrabold text-6xl container pt-10">This page illustrates the use of video with TailwindCSS.
+        </h1>
+        <h3 class="font-bold text-xl container">The video in the back is a loop of 250 frames saved as an mp4 file.
+        </h3>   
+        </video-background>
+    </ClientOnly>  
+   </section>>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+      <h1 class="pageTitle mt-10">This page illustrates the use of video in Nuxt.
+      </h1>
+      <div class="buttonBack font-bold ">
+          <NuxtLink to="https://github.com/avidofood/vue-responsive-video-background-player#readme" target="_blank">Click here for the plug in that is used</NuxtLink>
+      </div>
+      <div class="buttonBack font-bold mb-10 ">
+          <NuxtLink to="/example">Click here for the example</NuxtLink>
+      </div>
+  </div>
+</template>    
+
+<style>
+.buttonBack{
+  border: 1px solid #ccc;
+  border-radius: .2em;
+  margin-top: 1em;
+  padding: .75em;
+  width:max-content;
+  max-width: 40vw;
+}
+</style>

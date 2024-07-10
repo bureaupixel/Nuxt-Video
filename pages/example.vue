97e57cb63af2b708aeb767b55af2a30c6694a3c1
Bureaupixel <bram.overstijns@gmail.com>
2024-07-10 09:53:27 +0200

    Add files via upload

--- pages/example.vue
+++ pages/example.vue
@@ -8,11 +8,11 @@
          poster="/images/prev1.jpg"
          overlay="linear-gradient(to right, zinc-900, slate-900)"
          class="fixed top-0 left-0 w-full h-full overflow-hidden mt-5">
-         <h1 class="font-extrabold text-6xl container pt-10">This page illustrates the use of video with TailwindCSS.
+         <h1 class="font-extrabold text-6xl container pt-10">This page illustrates the use of video with Nuxt.
         </h1>
         <h3 class="font-bold text-xl container">The video in the back is a loop of 250 frames saved as an mp4 file.
         </h3>   
         </video-background>
     </ClientOnly>  
-   </section>>
+   </section>
 </template>
